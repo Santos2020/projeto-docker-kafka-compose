196767dfcc26dbf5fc6378cdbd5db1bcf1a533f9
Carlos Santos39 <novasintoniasp@gmail.com>
2022-07-19 11:32:36 -0300

    messagem enviada com sucesso Kafdrop

--- src/main/java/com/projeto/kafka/docker/strproducer/resources/StringProducerResource.java
+++ src/main/java/com/projeto/kafka/docker/strproducer/resources/StringProducerResource.java
@@ -1,2 +1,24 @@
-package com.projeto.kafka.docker.strproducer.resources;public class StringProducerResource {
+package com.projeto.kafka.docker.strproducer.resources;
+
+import com.projeto.kafka.docker.strproducer.services.StringProducerService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequiredArgsConstructor
+@RestController
+@RequestMapping(value = "/producer")
+public class StringProducerResource {
+
+    private final StringProducerService producerService;
+
+    @PostMapping
+    public ResponseEntity<?> sendMessage(@RequestBody String message) {
+        producerService.sendMessage(message);
+        return ResponseEntity.status(HttpStatus.CREATED).build();
+    }
 }
