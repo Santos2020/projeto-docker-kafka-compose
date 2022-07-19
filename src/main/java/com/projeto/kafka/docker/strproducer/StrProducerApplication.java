52d64290358421cfdb869e29d1284c7d80ef5c75
Carlos Santos39 <novasintoniasp@gmail.com>
2022-07-18 21:54:31 -0300

    Projeto kaka-docker criado

---
+++ src/main/java/com/projeto/kafka/docker/strproducer/StrProducerApplication.java
@@ -0,0 +1,13 @@
+package com.projeto.kafka.docker.strproducer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StrProducerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StrProducerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/projeto/kafka/docker/strproducer/StrProducerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projeto.kafka.docker.strproducer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StrProducerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
